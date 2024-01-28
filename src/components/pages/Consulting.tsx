3b9098ba05283548e5defab4d2d849533f2b0c80
Haileab <womahaileab1428@gmail.com>
2024-01-28 22:37:37 +0300

    finshed technology consultant section

--- src/components/pages/Consulting.tsx
+++ src/components/pages/Consulting.tsx
@@ -1,12 +1,16 @@
 import HeroSection from "../organs/Consulting/HeroSection"
 import Brands from "../organs/Consulting/Brands"
 import Services from "../organs/Consulting/Services"
+import Platform from "../organs/Consulting/Platforms"
+import FAQ from "../organs/Consulting/FAQ"
 const Consulting = ()=> {
     return (
         <>
             <HeroSection/>
             <Brands/>
             <Services/>
+            <Platform/>
+            <FAQ/>
         </>
     )
 }
---
+++ src/components/particles/ConsultacyData/FAQ.tsx
@@ -0,0 +1,24 @@
+export const DataOne = {
+    cards: [
+        {
+            firstText: "How do you determine the right technology solutions for our business?",
+            secondText: "We adopt a consultative approach by conducting a thorough analysis of your business requirements, existing technology infrastructure, and goals. Our consultants work closely with your team to understand your challenges and recommend tailored solutions. We consider factors like scalability, cost-effectiveness, compatibility, and industry best practices to determine the most suitable technology solutions for your business."
+        }, 
+        {
+            firstText: "How do you stay updated with the latest technology trends and advancements?",
+            secondText: "Our consultants regularly participate in industry events, training programs, and knowledge-sharing initiatives to stay updated on the latest technology trends, advancements, and best practices. This commitment to continuous learning ensures that we provide our clients with innovative and up-to-date solutions."
+        }, 
+        {
+            firstText: "Can you provide references or case studies of your past technology consultancy projects?",
+            secondText: "Certainly! We can provide references and share relevant case studies showcasing our successful technology consultancy projects. These examples will help you understand our expertise, methodologies, and the positive impact we have had on our clients' businesses."
+        },
+        {
+            firstText: "Can you help us implement the recommended technology solutions?",
+            secondText: "Absolutely! Our agency offers end-to-end technology consultancy services, including implementation support. We can assist you in project planning, solution design, vendor management, implementation oversight, and post-implementation support to ensure a smooth and successful transition to the new technology solutions."
+        }, 
+        {
+            firstText: " How do you ensure your consultants remain knowledgeable about the latest technology trends?",
+            secondText: "We prioritize continuous learning by encouraging our consultants to actively participate in industry conferences, seminars, and training programs. This commitment to professional development enables them to stay knowledgeable about the latest technology trends, ensuring our clients receive up-to-date and innovative solutions."
+        }
+    ]
+}
