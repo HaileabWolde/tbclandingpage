e6892a9d5fed0a56eb62918c4906b676ebd13613
Haileab <womahaileab1428@gmail.com>
2024-02-02 00:37:15 +0300

    finshed the testimonals part

--- src/components/organs/Software/Testimonals.tsx
+++ src/components/organs/Software/Testimonals.tsx
@@ -1,31 +1,22 @@
 import {Text} from "../../atoms/Text";
+import { useState } from "react";
 import { useCallback, useRef } from "react";
 import Slider from "react-slick";
 import { Button } from "../../atoms/Button";
 import { TestimonialTexts } from "../../particles/Testimonies"
 import { Card } from "../../molecules/Card";
-import { CaretDown, CaretUp } from "@phosphor-icons/react";
 import ProfileImg1 from "../../../assets/profile1.jpeg"
 import ProfileImg2 from "../../../assets/profile2.jpeg"
 import ProfileImg3 from "../../../assets/profile3.jpeg"
 import ProfileImg4 from "../../../assets/profile4.jpeg"
-import Feedback from "react-bootstrap/esm/Feedback";
+import "./Testimonals.css"
 
 const Testimonials = ()=> {
     const sliderRef = useRef<Slider | null>();
-
+    const [activeSlide, setActiveSlide] = useState(0);
+    const [middleSlide, setMiddleSlide] = useState(1);
     // Function for next button
-    const next = () => {
-        if (sliderRef.current) {
-            sliderRef.current.slickNext();
-        }
-    };
-    // function for previous button
-    const previous = () => {
-        if (sliderRef.current) {
-            sliderRef.current.slickPrev();
-        }
-    };
+    
 
     // Slider settings
     const settings = {
@@ -52,14 +43,31 @@ const Testimonials = ()=> {
                 return "";
         }
     }, [])
+    const Checkup = (index)=> {
+        if(index === 4){
+            const newINDEX = 0
+            return setMiddleSlide( newINDEX)
+        }
+        else{
+            return setMiddleSlide(index)
+        }
+    }
+    console.log(middleSlide)
     return (
         <section id="TESTIMONIALS" className="w-full mb-4">
           <main className="w-full  bg-gradient-to-b from-gray-100 to-gray-100 py-[100px]">
-            <Slider ref={(slider) => (sliderRef.current = slider)} {...settings} className="flex  justify-between w-[90%] mx-auto overflow-hidden">
+            
+                <Slider
+                    ref={(slider) => (sliderRef.current = slider)}
+                    {...settings}
+                    className="flex justify-between w-[90%] mx-auto overflow-hidden"
+                >
               {TestimonialTexts.feedBacks.map((feedback, index) => (
                 <Card
                   key={index}
-                  cardClass="pb-[100px] pt-6 shadow-md border-solid border-2  bg-white rounded-lg bg-opacity-20"
+                  cardClass={` ${ middleSlide === index
+                    ? " bg-opacity-100"
+                    : "disabled"} h-[400px] pt-6 shadow-md border-solid border-2  bg-white rounded-lg`}
                   imageAlt={feedback.person}
                   imageSrc={renderProfileImg(index)}
                   imageWrapperClass="w-[90px] h-[90px] rounded-[50%] overflow-hidden mx-auto"
@@ -86,14 +94,20 @@ const Testimonials = ()=> {
             
             </Slider>
             <Text
-              className=" flex "
+              className=" flex w-full  justify-center mt-8"
               >
               {
                   ['Mike taylor', 'Gina', 'Emma', 'Loveth'].map((item, index)=> (
                     <Button
                     key={item + index}
-                    className="w-3 h-3 rounded-[50%] bg-[white] m-1 cursor-pointer hover:bg-[#4169E1] transition-colors duration-200 ease-in-out p-3"
-                    onClick={next}
+                    className={`w-2 h-2 rounded-[50%] bg-[white] m-1 cursor-pointer ${
+                        activeSlide === index ? "bg-[#4169E1] rounded-lg pl-[14px] pr-[14px]" : ""
+                      } transition-colors duration-200 ease-in-out p-[6px]`}
+                      onClick={() => {
+                        sliderRef.current?.slickGoTo(index);
+                        setActiveSlide(index);
+                        Checkup(index + 1);
+                      }}
                     >
                       
                     </Button>
