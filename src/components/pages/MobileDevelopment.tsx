54c64d110b473c2c50d7d8eef8d14cbb706ac474
Haileab <womahaileab1428@gmail.com>
2024-02-02 17:14:19 +0300

    edited the mobile development section

--- src/components/pages/MobileDevelopment.tsx
+++ src/components/pages/MobileDevelopment.tsx
@@ -3,14 +3,18 @@ import Services from "../organs/Mobile/MobileService";
 import Brands from "../organs/Mobile/MobileBrands";
 import Platform from "../organs/Mobile/MobilePlatforms";
 import FAQ from "../organs/Mobile/MobileFAQ";
+import Testimonials from "../organs/Mobile/Testimonials";
+import NewsLetter from "../organs/Mobile/Contact";
 const Mobile_Development = ()=>{
     return (
       <>
       <HeroSection/>
-      <Brands/>
       <Services/>
+      <Brands/>
+      <Testimonials/>
       <Platform/>
       <FAQ/>
+      <NewsLetter/>
      
       </>
     )
