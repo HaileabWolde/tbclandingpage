120921c8896a06a6b26b30c7204d48c47e324fb1
Haileab <womahaileab1428@gmail.com>
2024-02-14 11:56:33 +0300

    finshed the manag services section

--- src/components/pages/Services.tsx
+++ src/components/pages/Services.tsx
@@ -1,10 +1,20 @@
 import HeroSection from "../organs/Services/HeroSection";
 import Services from "../organs/Services/Services";
+import Brands from "../organs/Services/Brands";
+import Testimonials from "../organs/Services/Testimonals";
+import Platform from "../organs/Services/Platforms";
+import FAQ from "../organs/Services/FAQ";
+import NewsLetter from "../organs/Services/Contact";
 const ManagingServices = ()=>{
     return (
        <>
        <HeroSection/>
        <Services/>
+       <Brands/>
+       <Testimonials/>
+       <Platform/>
+       <FAQ/>
+       <NewsLetter/>
        </>
     )
 }
---
+++ src/components/particles/ServicesData/Platforms.tsx
@@ -0,0 +1,24 @@
+export const DataOne = {
+    cards: [
+        {
+            firstText: " Why should I consider outsourcing IT management to a Managed Services provider?",
+            secondText: "Outsourcing IT management to a Managed Services provider offers several advantages. It allows you to leverage the expertise of specialized professionals who can efficiently manage your IT infrastructure, ensuring its reliability, security, and optimal performance. By offloading IT management, you can focus on your core business objectives, reduce overhead costs, and benefit from proactive monitoring and issue resolution."
+        }, 
+        {
+            firstText: "How can Managed Services benefit my business?",
+            secondText: "Managed Services offer several benefits for businesses, including access to a team of IT experts, proactive issue resolution, improved security measures, reduced downtime, cost savings, and the ability to focus on core business activities while leaving IT management to the experts."
+        }, 
+        {
+            firstText: "What types of services are typically included in Managed Services?",
+            secondText: "Managed Services typically include a range of services such as network monitoring and management, application support and maintenance, infrastructure management, security monitoring and incident response, data backup and recovery, software updates and patches, and IT help desk support."
+        },
+        {
+            firstText: " How is data security ensured with Managed Services?",
+            secondText: "Managed Services providers prioritize data security by implementing robust security measures. These can include firewalls, intrusion detection systems, regular security audits, vulnerability assessments, encryption, access controls, and employee training. Additionally, they stay updated with the latest security practices and regulations to protect against evolving threats."
+        }, 
+        {
+            firstText: "How do I know if Managed Services are right for my business?",
+            secondText: "Managed Services are suitable for businesses of all sizes and industries. If you want to minimize IT-related disruptions, improve security, access expert support, and free up internal resources to focus on core business functions, Managed Services can be a valuable solution. A consultation with a Managed Services provider can help assess your specific needs and determine the best approach for your business."
+        }
+    ]
+}
