5e4d19f6db27ee40f6946640be2b08cb22981381
Haileab <womahaileab1428@gmail.com>
2024-02-15 15:58:03 +0300

    changes

--- src/components/pages/Services.tsx
+++ src/components/pages/Services.tsx
@@ -10,7 +10,7 @@ const ManagingServices = ()=>{
        <>
        <HeroSection/>
        <Services/>
-       <Brands/>
+        {/*<Brands/>*/}
        <Testimonials/>
        {/*<Platform/>*/}
        <FAQ/>
