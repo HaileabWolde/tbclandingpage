ac69c4743c24125bbc823525c21bf4e4a4763393
Haileab <womahaileab1428@gmail.com>
2024-02-15 11:52:28 +0300

    changed The Testimonal Section

--- src/components/organs/Testimonials.tsx
+++ src/components/organs/Testimonials.tsx
@@ -1,39 +1,50 @@
+import {Text} from "../atoms/Text";
+import { useState } from "react";
 import { useCallback, useRef } from "react";
-import { Text } from "../atoms/Text"
-import { TestimonialTexts } from "../particles/Testimonies"
 import Slider from "react-slick";
 import { Button } from "../atoms/Button";
-import { CaretDown, CaretUp } from "@phosphor-icons/react";
+import { TestimonialTexts } from "../particles/Testimonies"
 import { Card } from "../molecules/Card";
-import ProfileImg1 from "../../assets/profile1.jpeg"
-import ProfileImg2 from "../../assets/profile2.jpeg"
-import ProfileImg3 from "../../assets/profile3.jpeg"
-import ProfileImg4 from "../../assets/profile4.jpeg"
+import ProfileImg1 from "../../assets/Engida.png"
+import ProfileImg2 from "../../assets/tbc_logo/TBCONE.png"
+import ProfileImg3 from "../../assets/Gafat.png"
+import ProfileImg4 from "../../assets/Excellence.png"
+import {  FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
-const Testimonials = () => {
+const Testimonials = ()=> {
     const sliderRef = useRef<Slider | null>();
-
+    const [activeSlide, setActiveSlide] = useState(0);
+    const [middleSlide, setMiddleSlide] = useState(1);
     // Function for next button
-    const next = () => {
-        if (sliderRef.current) {
-            sliderRef.current.slickNext();
-        }
-    };
-    // function for previous button
-    const previous = () => {
-        if (sliderRef.current) {
-            sliderRef.current.slickPrev();
-        }
-    };
+    
 
     // Slider settings
     const settings = {
         dots: false,
         infinite: true,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
-        vertical: true,
-        verticalSwiping: true,
+        swipeToSlide: true, // Allow swiping on mobile devices
+        adaptiveHeight: true,
+        responsive: [
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 2,
+              dots: false,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false,
+            },
+          },
+        ] // Adjust the height of the carousel to the current slide
 
     };
 
@@ -51,57 +62,90 @@ const Testimonials = () => {
                 return "";
         }
     }, [])
-
+    const Checkup = (index:number)=> {
+        if(index === 4){
+            const newINDEX = 0
+            return setMiddleSlide( newINDEX)
+        }
+        else{
+            return setMiddleSlide(index)
+        }
+    }
+    console.log(middleSlide)
     return (
-        <section id="TESTIMONIES" className="w-full flex  flex-col items-start justify-center relative lg:px-24 md:px-10 px-6  pt-2 md:pt-8 gap-5 ">
-            <main className='w-full grid md:grid-cols-1 lg:gap-0 gap-8 md:gap-5 '>
-                {/* Text and Steps Container  */}
+        <section id="TESTIMONIES" className="w-full">
+          <main className="w-full  bg-gradient-to-b from-gray-100 to-gray-100 py-[100px]">
                 <div className='w-full flex flex-col gap-6 '>
                     <Text as="p" className="font-semibold bg-gradient-to-r  from-[#4698CC]  to-[#4698CC] bg-clip-text text-transparent   text-center">
                         {TestimonialTexts.firstText}
                     </Text>
-                    <Text as="h1" className="lg:text-5xl md:text-3xl text-4xl  bg-gradient-to-r from-red-500 to-purple-500 text-transparent bg-clip-text font-medium text-center">
+                    <Text as="h1" className="text-xl md:text-4xl bg-gradient-to-r from-red-500 to-purple-500 text-transparent bg-clip-text font-semibold text-center mb-4">
                         {TestimonialTexts.secondText}
                     </Text>
                 </div>
-                {/* Testimonial Slides Container  */}
-                <div className="w-full lg:h-[400px] flex justify-center gap-4 items-center">
-                    <div className="lg:h-[250px] w-[90%]">
-                        <Slider ref={(slider) => (sliderRef.current = slider)} {...settings}>
-                            {
-                                TestimonialTexts.feedBacks.map((feedBack, index) => (
-                                    <div className="w-full">
-                                        <Card key={index} cardClass=" shadow border-[1px] border-color3/10 relative rounded-xl pl-4 pr-4  pb-6 lg:h-[200px] h-[260px] lg:mb-4 w-full flex flex-col gap-8 md:gap-4 justify-center items-center mx-auto" imageAlt={feedBack.person} imageSrc={renderProfileImg(index)} imageWrapperClass=" rounded-[50%] overflow-hidden" cover="object-cover object-top" textWrapperClass="flex flex-col justify-center gap-6 mt-2 md:mt-0">
-                                            <Text as="q" className=" mt-10 md:mt-0 text-[0.84rem] font-light text-color3">
-                                                {feedBack.text}
-                                            </Text>
-                                            <div className="flex flex-col gap-2 absolute md:top-[48px] top-[100px] left-[100px]">
-                                                <Text as="h4" className="text-base text-color3 font-medium">
-                                                    {feedBack.person}
-                                                </Text>
-                                                <Text as="p" className="text-sm text-color3 font-light">
-                                                    {feedBack.location}
-                                                </Text>
-                                            </div>
-                                        </Card>
-                                    </div>
-                                ))
-                            }
-                        </Slider>
-                    </div>
-                    {/* Controllers  */}
-                    <div className="flex flex-col gap-4 pb-5">
-                        <Button onClick={previous} id="prev" className="cursor-pointer outline-none border-none bg-color2/30 text-color3 hover:bg-color2 p-2 rounded-full" type="button">
-                            <CaretUp size={18} color="currentColor" weight="fill" />
-                        </Button>
-                        <Button onClick={next} id="next" className="cursor-pointer outline-none border-none bg-color2/30 text-color3 hover:bg-color2 p-2 rounded-full" type="button">
-                            <CaretDown size={18} color="currentColor" weight="fill" />
-                        </Button>
-                    </div>
-                </div>
-            </main>
+                <Slider
+                    ref={(slider) => (sliderRef.current = slider)}
+                    {...settings}
+                    className="flex justify-between w-[90%] mx-auto overflow-hidden"
+                >
+              {TestimonialTexts.feedBacks.map((feedback, index) => (
+                <Card
+                  key={index}
+                  cardClass={` ${ middleSlide === index
+                    ? " bg-opacity-100 transform scale-110"
+                    : " bg-opacity-100 md:opacity-50 md:pointer-events-none"} h-[400px] pt-6 shadow-md border-solid border-2  bg-white rounded-lg`}
+                  imageAlt="Hello"
+                  imageSrc={renderProfileImg(index)}
+                  imageWrapperClass="w-[35%] h-[35%] overflow-hidden mx-auto"
+                  cover="object-fit"
+                >
+                    <Text
+                    className="w-[70%] mx-auto flex flex-col items-center mt-4 gap-2 "
+                    >
+                        
+                        <Text>
+                            <h4 className="font-semibold font-serif">{feedback.location}</h4>
+                        </Text>
+                        <Text>
+                      
+                          <FaQuoteLeft className={`${middleSlide === index ? 'text-gray-600': 'text-gray-300'} mb-2`}/>
+                            <h4 className="font-serif text-sm">{feedback.text}</h4>
+                            <div className="w-full flex flex-col items-end">
+                            <FaQuoteRight className={`${middleSlide === index ? 'text-gray-600': 'text-gray-300'} mb-2`}/>
+                            </div>
+                           
+                        </Text>
+                    </Text>
+                   
+                </Card>
+              ))}
+            
+            </Slider>
+            <Text
+              className=" flex w-full  justify-center mt-8"
+              >
+              {
+                  ['Mike taylor', 'Gina', 'Emma', 'Loveth'].map((item, index)=> (
+                    <Button
+                    key={item + index}
+                    className={`w-2 h-2 rounded-[50%] m-1 cursor-pointer ${
+                        activeSlide === index ? "bg-[#4698CC] rounded-lg pl-[14px] pr-[14px]" : " bg-gradient-to-r from-red-200 to-red-500"
+                      } transition-colors duration-200 ease-in-out p-[6px]`}
+                      onClick={() => {
+                        sliderRef.current?.slickGoTo(index);
+                        setActiveSlide(index);
+                        Checkup(index + 1);
+                      }}
+                    >
+                      
+                    </Button>
+                  ))
+                }
+              </Text>
+          </main>
         </section>
-    )
+      );
 }
-
-export default Testimonials
+export default Testimonials;
+/*
+*/
--- src/components/particles/Testimonies.tsx
+++ src/components/particles/Testimonies.tsx
@@ -3,24 +3,20 @@ export const TestimonialTexts = {
     secondText: "What People Say About Us",
     feedBacks: [
         {
-            text: "You did a fabulous job. There were no hitches. Thank you again for all your work planning this trip.",
-            person: "Mike taylor",
-            location: "Lahore, Pakistan"
+            text: "TBC Technology Solutions' mobile app is a game-changer! It's intuitive, sleek, and has significantly improved our productivity.",
+            location: "TBC TECHNOLOGY"
         },
         {
-            text: "TBC Technologies was great with the entire process from planning to updates during the trip. We had 11 people and everything was perfectly executed. We appreciate all of her hard work. It was truly the trip of a lifetime. Thank you!",
-            person: "Gina",
-            location: "Owerri, Nigeria"
+            text: "Engidiea's bus ticket booking app is a true gem! It's seamless, reliable, and has made our travel experience hassle-free.",
+            location: "ENGIDIEA SOLUTION"
         },
         {
-            text: "Booking through you was very easy and made our lives so much easier. I have nothing bad to say! Thank you for giving us tips and guidance before we left on what to bring and such, that was very helpful!",
-            person: "Emma",
-            location: "Vancouver, Canada"
+            text: "The mobile application developed by TBC Technology Solutions is nothing short of brilliant. It's user-friendly, feature-rich, and has exceeded our expectations.",
+            location: "GAFAT TECHNOLOGY"
         },
         {
-            text: "Thank you for your recommendation and putting the trip together. The resort was absolutely beautiful. The infinity pools, the palm trees in the main pool, the infinity pool in out preferred area overlooking the gulf and the golf course were exceptional....",
-            person: "Loveth",
-            location: "Lagos, Nigeria"
+            text: "TBC Technology Solutions has delivered an exceptional mobile app that has revolutionized our operations. It's a perfect blend of functionality, design, and reliability.",
+            location: "EXCELLENCE CONSULTANCY"
         },
     ],
 }
